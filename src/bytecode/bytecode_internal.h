5281549d46249df4a869ef047c31cd113a15d429
Jakob Bowyer <jkbbwr@gmail.com>
2019-04-05 16:55:05 +0100

    WIP intrinsics

--- src/bytecode/bytecode_internal.h
+++ src/bytecode/bytecode_internal.h
@@ -15,14 +15,17 @@ extern "C" {
 int bytecode_read_uint32(uint32_t* num, const uint8_t* raw, size_t size, size_t* offset);
 int bytecode_read_string(char* str, size_t str_size, const uint8_t* raw, size_t size, size_t* offset);
 int bytecode_read_uuid(uint8_t* uuid, const uint8_t* raw, size_t size, size_t* offset);
+int bytecode_read_intrinsic(intrinsic_t* intrinsic, const uint8_t* raw, size_t size, size_t* offset);
 
 int bytecode_read_string_constants(bytecode_t* bytecode, const uint8_t* raw, size_t size, size_t* offset);
 int bytecode_read_integer_constants(bytecode_t* bytecode, const uint8_t* raw, size_t size, size_t* offset);
 int bytecode_read_artifact_constants(bytecode_t* bytecode, const uint8_t* raw, size_t size, size_t* offset);
+int bytecode_read_intrinsic_constants(bytecode_t* bytecode, const uint8_t* raw, size_t size, size_t* offset);
 
 void bytecode_dispose_string_constants(bytecode_t* bytecode);
 void bytecode_dispose_integer_constants(bytecode_t* bytecode);
 void bytecode_dispose_artifact_constants(bytecode_t* bytecode);
+void bytecode_dispose_intrinsic_constants(bytecode_t* bytecode);
 
 #ifdef __cplusplus
 }
---
+++ src/bytecode/bytecode_read_intrinsic_constants.c
@@ -0,0 +1,4 @@
+//
+// Created by jakob on 05/04/19.
+//
+
